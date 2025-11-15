a8f231bbb4e93e7125c4382ddcfeded7554a6fa3
Doğancan Kaygusuz <dkaygusuz1025@gmail.com>
2025-11-15 16:06:53 +0300

    slices

---
+++ functions/function1.go
@@ -0,0 +1,14 @@
+package functions
+
+func Topla(sayi1 int, sayi2 int) int {
+	return sayi1 + sayi2
+}
+
+func DortIslem(sayi1 int, sayi2 int) (int, int, int, float32) {
+	toplam := sayi1 + sayi2
+	fark := sayi1 - sayi2
+	carpim := sayi1 * sayi2
+	bolum := float32(sayi1 / sayi2)
+
+	return toplam, fark, carpim, bolum
+}
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"golesson/arrays"
 	"golesson/conditionals"
+	"golesson/functions"
 	"golesson/loops"
 	"golesson/slices"
 	"golesson/variables"
@@ -16,4 +18,23 @@ func main() {
 	arrays.ArraysDemo2()
 	arrays.ArraysDemo3()
 	slices.SlicesDemo()
+	fmt.Println(functions.Topla(5, 12))
+	fmt.Println(functions.DortIslem(24, 3))
+
+	sonuc1, sonuc2, sonuc3, sonuc4 := functions.DortIslem(25, 5)
+	sonuc11, sonuc22, _, sonuc44 := functions.DortIslem(12, 3)
+	fmt.Println(sonuc1)
+	fmt.Println(sonuc2)
+	fmt.Println(sonuc3)
+	fmt.Println(sonuc4)
+	fmt.Println(sonuc11)
+	fmt.Println(sonuc22)
+	fmt.Println(sonuc44)
+
+	fmt.Println("VARIADIC")
+	toplam := functions.ToplaVariadic(5, 6, 71, 5, 2, 94)
+	fmt.Println(toplam)
+
+	sayilar := []int{4, 52, 34, 14, 5, 12}
+	fmt.Println(functions.ToplaVariadic(sayilar...))
 }
